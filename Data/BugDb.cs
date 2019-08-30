095e8df5785c8e1e311323ca39f6a452a11e9dcb
Mojtaba Zolfaghari <mzolfaghari71@yahoo.com>
2019-08-30 18:52:49 +0430

    Add Virtual Keyboard

---
+++ Data/BugDb.cs
@@ -0,0 +1,14 @@
+﻿using BugVue.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugVue.Data
+{
+    public class BugDb:DbContext
+    {
+        public BugDb(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Bug> Bugs { get; set; }
+    }
+}
